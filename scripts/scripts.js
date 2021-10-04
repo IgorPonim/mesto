ca2796bb64237e61281f41540cb5a6c44a9464de
IgorPonim <igorponim@yandex.ru>
2021-10-04 10:31:41 +0300

    check

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,16 +3,11 @@ const ProfileInfo = document.querySelector('.profile__edit');
 const popupCloseButton = document.querySelector('.popup__close');
 const popupButton = document.querySelector('.popup__button');
 const form = document.querySelector('.popup__form');
-
 const nameField = document.querySelector('.popup__inputName');
 const Profile = document.querySelector('.profile__name');
-
 const Job = document.querySelector('.popup__inputJob');
 const Status = document.querySelector('.profile__status');
-
 /*const LikeHeart = document.querySelector('.element__reaction')  /// это с массивами не работает */
-
-
 
 /*мы еще не проходили forEach, помог разобраться старший студент*/
 function heart(ev) {
@@ -21,18 +16,11 @@ function heart(ev) {
 Array.from(document.querySelectorAll('.element__reaction')).forEach((el) => el.addEventListener('click', heart));
 
 
-
-
-
-
-
 function popupOpen() { popup.classList.add('popup_open') }
 function popupClose() { popup.classList.remove('popup_open') }
 
 ProfileInfo.addEventListener('click', popupOpen)
-
 popupCloseButton.addEventListener('click', popupClose)
-
 popupButton.addEventListener('click', popupClose)
 
 
@@ -46,8 +34,7 @@ function submitForm(event) {
 
 form.addEventListener('submit', submitForm)
 
-
-
+/*я так и не понял изз задания нужно єто через js делать или css*/
 let string = document.querySelector('.profile__name').textContent
-let shortText=string.substring(10, string.length - 255 );
-document.querySelector('.profile__name').textContent = shortText + "..."  ;
+let shortText = string.substring(10, string.length - 255);
+document.querySelector('.profile__name').textContent = shortText + "...";
