17bd13339f2cd864942893e6a05ed8afe6bdd1a9
IgorPonim <igorponim@yandex.ru>
2021-10-08 16:15:24 +0300

    hotfixes4

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,19 +3,20 @@ const profileInfo = document.querySelector('.profile__edit');
 const popupCloseButton = document.querySelector('.popup__close');
 const popupButton = document.querySelector('.popup__button');
 const form = document.querySelector('.popup__form');
-const nameField = document.querySelector('.popup__input_name');
+const nameField = document.querySelector('.popup__input_type_name');
 const profile = document.querySelector('.profile__name');
-const job = document.querySelector('.popup__input_job');
+const job = document.querySelector('.popup__input_type_job');
 const myStatus = document.querySelector('.profile__status');
 
 
-function popupOpen() { popup.classList.add('popup_open') }
+function popupOpen() {
+    popup.classList.add('popup_open');
+    nameField.value = profile.textContent;
+    job.value = myStatus.textContent;
+}  //ну вроде так?//
 function popupClose() { popup.classList.remove('popup_open') }
 
 profileInfo.addEventListener('click', popupOpen);
-//я надеюсь вас правильно понял?//
-profile.textContent = nameField.value;
-myStatus.textContent = job.value;
 popupCloseButton.addEventListener('click', popupClose);
 
 
