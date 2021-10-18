bab16ec038b4bd5a2f65d0a16813a071ebc90558
IgorPonim <igorponim@yandex.ru>
2021-10-18 20:15:08 +0300

    BugFix2_js_optimized

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
-const popupProfile = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup_type_profile');
+const popupElement = document.querySelector('.popup_type_element');
+const popupImage = document.querySelector('.popup_type_image')
 const profileButtonInfo = document.querySelector('.profile__edit');
 const popupCloseButton = document.querySelector('.popup__close');
 const profileForm = document.querySelector('.popup__form');
@@ -9,19 +11,20 @@ const myStatus = document.querySelector('.profile__status');
 
 //основыне функции - открыть и закрыть попап
 function popupOpen(evt) {
-    evt.classList.add('popup_open');
+  evt.classList.add('popup_open');
 }
 
-function popupClose(evt) { 
-    evt.classList.remove('popup_open') }
+function popupClose(evt) {
+  evt.classList.remove('popup_open')
+}
 
 function profileEditHandler() {
-    nameInputValue.value = profileName.textContent;
-    profileJob.value = myStatus.textContent;
-    popupOpen(popupProfile);
+  nameInputValue.value = profileName.textContent;
+  profileJob.value = myStatus.textContent;
+  popupOpen(popupProfile);
 }
 function profileCloseHandler() {
-    popupClose(popupProfile);
+  popupClose(popupProfile); //все как вы хотели))
 }
 //обработчики событий
 profileButtonInfo.addEventListener('click', profileEditHandler);
@@ -30,18 +33,19 @@ profileForm.addEventListener('submit', handleProfileSubmit)
 
 /*переносим данные из полей ввода в профиль*/
 function handleProfileSubmit(event) {
-    event.preventDefault()
-    profileName.textContent = nameInputValue.value;
-    myStatus.textContent = profileJob.value;
-    popupClose(popupProfile);
+  event.preventDefault()
+  profileName.textContent = nameInputValue.value;
+  myStatus.textContent = profileJob.value;
+  popupClose(popupProfile);
 }
 
 /*это чтобы закрывался попап не только после нажатия на крестик но и на оверлей*/
 function closePopupByOverlay(event) {
-    if (event.target.classList.contains('popup')) {
-        popupClose(popupProfile);
-        popupClosePlace();
-    }
+  if (event.target.classList.contains('popup')) {
+    popupClose(popupProfile);
+    elementCloseHandler(popupElement);
+    ImageCloseHandler(popupImage);
+  }
 }
 popupProfile.addEventListener('click', closePopupByOverlay);
 
@@ -52,30 +56,30 @@ popupProfile.addEventListener('click', closePopupByOverlay);
 //////////////////////////////////////////////////////////////////////////все что ниже это ПР5
 
 const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
 ];
 //теперь я выберу исходную секцию, куда добавляю карточки, ну и шаблон/темплате
 const elements = document.querySelector('.elements')
@@ -83,22 +87,30 @@ const cardTemplate = document.querySelector('.template').content;
 
 
 ///Сразу добавляем карточки при запуске страниці//
-function createElements(data) {
-    const element = cardTemplate.querySelector('.element').cloneNode(true);
-    element.querySelector('.element__name').textContent = data.name;
-    element.querySelector('.element__image').src = data.link;
-    element.querySelector('.element__image').alt = data.name;
-    element.querySelector('.element__delete').addEventListener('click', (ev) => {
-        ev.target.closest('.element').remove()
-    })
-    element.querySelector('.element__reaction').addEventListener('click', (ev) => {
-        ev.target.classList.toggle('element__reaction_like')
-    })
-    elements.append(element);
-    element.querySelector('.element__image').addEventListener('click', popupOpenImage);
+initialCards.forEach(prependElements);
 
+function createElements(data) {
+  const element = cardTemplate.querySelector('.element').cloneNode(true);
+  element.querySelector('.element__name').textContent = data.name;
+  element.querySelector('.element__image').src = data.link;
+  element.querySelector('.element__image').alt = data.name;
+  element.querySelector('.element__delete').addEventListener('click', (ev) => {
+    ev.target.closest('.element').remove()
+  })
+  element.querySelector('.element__reaction').addEventListener('click', (ev) => {
+    ev.target.classList.toggle('element__reaction_like')
+  })
+  /*elements.append(element);*/
+  element.querySelector('.element__image').addEventListener('click', popupOpenImage);
+  return element;
 };
-initialCards.forEach(createElements);
+//////как же тяжело было эту конструкцию соорудить
+function prependElements(item) {
+  const element = createElements(item);
+  elements.prepend(element)
+}
+
+
 
 //////////////////////////////////////////////////////
 //открываем и закрываем попап, по аналогии с тем, что касается профиля
@@ -106,17 +118,14 @@ const addButtonPlace = document.querySelector('.profile__add');
 const newCard = document.querySelector('.popup_type_element');
 const closeButtonInfoCard = document.querySelector('.popup__close_type_element')
 
-function popupOpenPlace() {
-    newCard.classList.add('popup_open');
+function elementEditHandler() {
+  popupOpen(popupElement);//все как вы хотели))
 }
-
-function popupClosePlace() {
-    newCard.classList.remove('popup_open');
+function elementCloseHandler() {
+  popupClose(popupElement);
 }
-
-addButtonPlace.addEventListener('click', popupOpenPlace);
-closeButtonInfoCard.addEventListener('click', popupClosePlace);
-
+addButtonPlace.addEventListener('click', elementEditHandler);
+closeButtonInfoCard.addEventListener('click', elementCloseHandler);
 //закрываем попап не только по крестику
 newCard.addEventListener('click', closePopupByOverlay);
 
@@ -126,23 +135,16 @@ newCard.addEventListener('click', closePopupByOverlay);
 const nameOfNewElement = document.querySelector('.popup__input_type_name-element');
 const linkOfNewElement = document.querySelector('.popup__input_type_link');
 const elementReactionLike = document.querySelector('.element__reaction')
-//это функция создания новой карточки, по аналогии с загрузкой исходных шести
+//пользователь создаст свою карточку на основе шаблона из prependElements и будут внесены данные из формы
 function addNewElement(event2) {
-    const element = cardTemplate.querySelector('.element').cloneNode(true);
-    element.querySelector('.element__name').textContent = nameOfNewElement.value;
-    element.querySelector('.element__image').src = linkOfNewElement.value;
-    element.querySelector('.element__delete').addEventListener('click', (ev) => {
-        ev.target.closest('.element').remove()
-    });
-    element.querySelector('.element__reaction').addEventListener('click', (ev) => {
-        ev.target.classList.toggle('element__reaction_like')
-    })
-    element.querySelector('.element__image').alt = nameOfNewElement.value;
-    element.querySelector('.element__image').addEventListener('click', popupOpenImage);
-    elements.prepend(element);
-    event2.preventDefault();
-    popupClosePlace();
 
+  const newValues = {
+    name: nameOfNewElement.value,
+    link: linkOfNewElement.value,
+  }
+  prependElements(newValues)
+  event2.preventDefault()
+  elementCloseHandler()
 };
 
 const buttonCreateCard = document.querySelector('.popup__button_type_create');
@@ -150,26 +152,23 @@ const popupFormImage = document.querySelector('.popup__form_type_image');
 popupFormImage.addEventListener('submit', addNewElement)
 
 //////////////////////////////теперь последний попап с картинкой
-/*const imagePush = document.querySelector('.element__image');*/
-const bigImage = document.querySelector('.popup_type_image');
+
+
 const imageInsidePopup = document.querySelector('.image-container__img');
 const imageCloseButton = document.querySelector('.popup__close_type_img');
 const imageInformation = document.querySelector('.image-container__info')
-//создаю карточку и переношу в нее ссылки на картинки и прочие атрибуты
+//открываю карточку и переношу в нее ссылки на картинки и прочие атрибуты
 function popupOpenImage(evt) {
-    const bigImage = document.querySelector('.popup_type_image');
-    bigImage.classList.add('popup_open');
-    imageInsidePopup.src = evt.target.src;
-    /* imageInformation.textContent = evt.target.parentElement.querySelector('.element__name').textContent;*/
-    imageInformation.textContent = evt.target.closest('.element__image').alt
+  popupOpen(popupImage);
+  imageInsidePopup.src = evt.target.src;
+  imageInformation.textContent = evt.target.parentElement.querySelector('.element__name').textContent;
 }
 
-
-function popupCloseLargeImage() {
-    bigImage.classList.remove('popup_open');
+function ImageCloseHandler() {
+  popupClose(popupImage)
 }
-imageCloseButton.addEventListener('click', popupCloseLargeImage);
-
+imageCloseButton.addEventListener('click', ImageCloseHandler);
+popupImage.addEventListener('click', closePopupByOverlay)
 
 
 
