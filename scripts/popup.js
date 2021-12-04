34bfd8e6f609471e3f9048cf5be9fdb32b204219
IgorPonim <igorponim@yandex.ru>
2021-12-04 20:08:01 +0300

    ПР8+before_webpack

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,14 +1,13 @@
 export class Popup {
   constructor(popup) {
     this._popup = popup
-
   }
-
 
   close() {
     this._popup.classList.remove('popup_open')
     document.removeEventListener('keydown', (evt) => this._handleEscClose(evt))//без стрелочной функции не работает. Почему? непонятно...
   }
+
   open() {
     this._popup.classList.add('popup_open');
     document.addEventListener('keydown', (evt) => this._handleEscClose(evt))
@@ -20,7 +19,6 @@ export class Popup {
     }
   }
 
-
   setEventListeners() {//хитроумная конструкция, решил сохранить с предыдущей ПР7
     this._popup.addEventListener('click', (evt) => {
       if (evt.target.classList.contains('popup_open')) {
@@ -30,8 +28,5 @@ export class Popup {
         this.close()
       }
     })
-
-
   }
-
 }
