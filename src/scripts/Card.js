aa6658ee7d2ef18739044f02816b93c56be17eed
IgorPonim <igorponim@yandex.ru>
2021-12-22 18:05:53 +0300

    PR9_likes_amount

--- src/scripts/Card.js
+++ src/scripts/Card.js
@@ -1,14 +1,24 @@
 
 
 export class Card {
-  constructor(data, template, handleCardClick) {
+  constructor(data, template, handleCardClick, sendLike, deleteLike, id) {
     this._name = data.name,
       this._link = data.link,
       this._template = template;
+    this._likes = data.likes
     this._popupImage = document.querySelector('.popup_type_image');
     this._imageInsidePopup = this._popupImage.querySelector('.image-container__img');
     this._imageInformation = this._popupImage.querySelector('.image-container__info')
     this._handleCardClick = handleCardClick//это просто метод popup.open, который работает через стрелочную функцию....
+    this._sendLike = sendLike
+    this._deleteLike = deleteLike
+    this._mainTemplate = this._getTemplate()
+    this.isLiked = data.isLiked;
+    this._buttonLike = this._mainTemplate.querySelector('.element__reaction')
+    this._id = data._id;
+    this._userId = id;
+    this._amountOfLikes = this._mainTemplate.querySelector('.element__reaction-amount')
+
   }
 
   //выбрал свой шаблон
@@ -26,33 +36,51 @@ export class Card {
 
   //универсальный метод создания карточки
   createElement() {
-    this._mainTemplate = this._getTemplate()
+
     this._elementImage = this._mainTemplate.querySelector('.element__image');
     this._elementImage.src = this._link
     this._elementImage.alt = this._name
     this._mainTemplate.querySelector('.element__name').textContent = this._name
     this._setEventListener();
+
+
+
     return this._mainTemplate
   }
 
-  _handeLikeClick(ev) {//могу я использовать творческий подход?
-    ev.target.classList.toggle('element__reaction_like')
+  _handeLikeClick() {
+    const checkLike = this._likes.some(({ _id }) => _id === this._userId); //проверю есть ли мои лайки
+    if (checkLike === true) {
+      this._deleteLike(this._id).then(this._updateLikes).catch((err) => console.log(err));
+    } else {
+      this._sendLike(this._id).then(this._updateLikes).catch((err) => console.log(err));
+    }
   }
   _handeDeleteClick() {
-    this._mainTemplate.remove()//все как вы сказали
-    this._mainTemplate = null //когда вы говорите 'зануллить' - это это?
+    this._mainTemplate.remove()
+    this._mainTemplate = null
   }
 
+  _updateLikes = ({ likes }) => {//объектный литерал
+    this._buttonLike.classList.toggle('element__reaction_like');
 
+    this._likes = likes;
+    this._amountOfLikes.textContent = this._likes.length;
+  }
 
   //вешаю слушатели
   _setEventListener() {
-    this._mainTemplate.querySelector('.element__reaction').addEventListener('click', (ev) => {
+    this._buttonLike.addEventListener('click', (ev) => {
       this._handeLikeClick(ev)
     })
     this._mainTemplate.querySelector('.element__delete').addEventListener('click', (ev) => {
       this._handeDeleteClick(ev)
     })
     this._elementImage.addEventListener('click', () => { this._handleCardClick() })
+    const checkLike = this._likes.some(({ _id }) => _id === this._userId);
+    if (checkLike === true) {
+      this._buttonLike.classList.add('element__reaction_like');
+    }
+    this._amountOfLikes.textContent = this._likes.length;
   }
 }
--- src/scripts/UserInfo.js
+++ src/scripts/UserInfo.js
@@ -13,9 +13,10 @@ export class UserInfo {
     }
   }
 
-  setUserInfo({ name, about, avatar }) { //раньше второй аргумент был job теперь about, ибо так настроили сервер
+  setUserInfo({ name, about, avatar, _id }) { //раньше второй аргумент был job теперь about, ибо так настроили сервер
     this._name.textContent = name;
     this._job.textContent = about;
     this._avatar.src = avatar;
+    this.id = _id;
   }
 }
